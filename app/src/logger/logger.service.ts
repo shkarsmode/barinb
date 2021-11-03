f992cf7daefea0d413b418e28c83d5a0b6386e36
shkarsmode <zshkarrr@gmail.com>
2021-11-03 16:47:07 +0200

    next breakpoint

--- app/src/logger/logger.service.ts
+++ app/src/logger/logger.service.ts
@@ -17,3 +17,54 @@ export class LoggerService {
     console.info(payload);
   }
 }
+
+// Вне angular
+/*
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+
+import { ConfigService } from '@app/core/config';
+
+export interface LoggerData {
+  context: Record<string, any>;
+  level: string;
+  message: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoggerService {
+  private readonly url: string;
+
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject(PLATFORM_ID) private readonly platformId: any
+  ) {
+    this.url = `${this.configService.getConfig().apiUrl}/logger`;
+  }
+
+  log(data: Partial<LoggerData> = {}): void {
+    if (isPlatformBrowser(this.platformId) && window['XMLHttpRequest'] !== null) {
+      data.context = { ...data.context };
+
+      if (!data.level) {
+        data.level = 'warning';
+      }
+      if (!data.message) {
+        data.message = 'JS unhandled error';
+      }
+      if (data.context?.error && !data.context?.stack) {
+        data.context.stack = data.context.error?.stack?.toString();
+      }
+
+      const xhr = new XMLHttpRequest();
+      xhr.open('POST', this.url, true);
+      xhr.setRequestHeader('Content-type', 'application/json');
+      xhr.setRequestHeader('Accept', 'application/json');
+      xhr.send(JSON.stringify(data));
+    }
+  }
+
+}
+*/
